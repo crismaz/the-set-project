6952e0dde85012cf504a81663f41fc31dea4938e
crismaz <krzysztof_maziarz@onet.pl>
2015-08-11 18:50:12 +0200

    Moved appropriate functionalities to SetFigure

--- app/src/main/java/krzysztofmaziarz/thesetproject/core/ImageProcessor.java
+++ app/src/main/java/krzysztofmaziarz/thesetproject/core/ImageProcessor.java
@@ -7,7 +7,6 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
@@ -20,34 +19,42 @@ public class ImageProcessor {
     private static final int DEFAULT_MAT_TYPE = CvType.CV_8U;
     private static final double CANNY_LOWER_THRESHOLD = 100.0;
     private static final double CANNY_UPPER_THRESHOLD = 250.0;
+    private static final double MIN_CONTOUR_SIZE = 0.05;
 
     public static Bitmap markSets(Bitmap source) {
         int imageHeight = source.getHeight();
         Mat imageMat = new Mat(source.getHeight(), source.getWidth(), DEFAULT_MAT_TYPE);
         Utils.bitmapToMat(source, imageMat);
 
-        Mat edges = new Mat(imageMat.rows(), imageMat.cols(), DEFAULT_MAT_TYPE);
-        Imgproc.Canny(imageMat, edges,
-                CANNY_LOWER_THRESHOLD,
-                CANNY_UPPER_THRESHOLD);
+        Mat edges = getEdges(imageMat);
 
         List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
 
-        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+        List<SetFigure> figures = new ArrayList<>();
 
         for (MatOfPoint contour : contours) {
-            if (normalize(contour.rows(), imageHeight) > 0.05) {
-                Rect box = Imgproc.boundingRect(contour);
+            if (normalize(contour.rows(), imageHeight) > MIN_CONTOUR_SIZE) {
+                SetFigure figure = new SetFigure(imageMat, contour);
 
-                if (SetFigure.checkRatio(box.height, box.width)) {
-                    Core.rectangle(imageMat, box.tl(), box.br(), new Scalar(1));
+                if (figure.isValid()) {
+                    figures.add(figure);
+                    Core.rectangle(imageMat, figure.getBox().tl(), figure.getBox().br(), new Scalar(1));
                 }
             }
         }
 
         Utils.matToBitmap(imageMat, source);
         return source;
+    }
+
+    private static Mat getEdges(Mat imageMat) {
+        Mat edges = new Mat(imageMat.rows(), imageMat.cols(), DEFAULT_MAT_TYPE);
+        Imgproc.Canny(imageMat, edges,
+                CANNY_LOWER_THRESHOLD,
+                CANNY_UPPER_THRESHOLD);
+
+        return edges;
     }
 
     private static double normalize(int value, int imageHeight) {
--- app/src/main/java/krzysztofmaziarz/thesetproject/model/SetFigure.java
+++ app/src/main/java/krzysztofmaziarz/thesetproject/model/SetFigure.java
@@ -2,26 +2,66 @@ package krzysztofmaziarz.thesetproject.model;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 
 public class SetFigure {
     private static final double MIN_RATIO = 1.5;
     private static final double MAX_RATIO = 3.0;
 
-    Mat image;
-    MatOfPoint contour;
+    private Mat image;
+    private MatOfPoint contour;
+    private Rect box;
 
-    boolean valid;
+    private boolean valid;
 
-    Color color;
-    Shading shading;
-    Shape shape;
+    private Color color;
+    private Shading shading;
+    private Shape shape;
 
     public SetFigure(Mat image, MatOfPoint contour) {
         this.image = image;
         this.contour = contour;
+
+        initialize();
     }
 
-    public static boolean checkRatio(int height, int width) {
+    private void initialize() {
+        box = Imgproc.boundingRect(contour);
+        computeValid();
+
+        if (valid) {
+            computeColor();
+            computeShading();
+            computeShape();
+        }
+    }
+
+    private void computeValid() {
+        valid = checkRatio(box.height, box.width);
+    }
+
+    private void computeColor() {
+        // TODO
+    }
+
+    private void computeShading() {
+        // TODO
+    }
+
+    private void computeShape() {
+        // TODO
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public Rect getBox() {
+        return box;
+    }
+
+    private static boolean checkRatio(int height, int width) {
         return  height >= width * MIN_RATIO &&
                 height <= width * MAX_RATIO;
     }
